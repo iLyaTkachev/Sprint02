307bca21b157f8b59bd66274f5efc142f472916b
iLya Tkachev <ilya@iLyas-Mac.local>
2017-04-06 11:56:55 +0300

    add screen for cell

---
+++ Sprint01ARC/CustomViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomViewController.h
+//  Sprint01ARC
+//
+//  Created by iLya Tkachev on 4/6/17.
+//  Copyright © 2017 iLya Tkachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomViewController : UIViewController
+
+@end
---
+++ Sprint01ARC/CustomViewController.m
@@ -0,0 +1,37 @@
+//
+//  CustomViewController.m
+//  Sprint01ARC
+//
+//  Created by iLya Tkachev on 4/6/17.
+//  Copyright © 2017 iLya Tkachev. All rights reserved.
+//
+
+#import "CustomViewController.h"
+
+@interface CustomViewController ()
+
+@end
+
+@implementation CustomViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
